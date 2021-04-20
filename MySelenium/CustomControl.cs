533817dc28f1fa7840b536d931dce5a4ffc5c0dd
Tharvez <abdulhakeem.h@gmail.com>
2021-04-20 09:35:03 -0700

    Initial Selenium .Net Core upto customcontrol

---
+++ MySelenium/CustomControl.cs
@@ -0,0 +1,18 @@
+﻿
+
+using OpenQA.Selenium;
+
+namespace MySelenium
+{
+    public class CustomControl: DriverHelper
+    {
+        public static void comboControl(string controlName, string value)
+        {
+            IWebElement comboBox = driver.FindElement(By.XPath($"//input[@id='{controlName}-awed']"));
+            comboBox.Clear();
+            comboBox.SendKeys(value);
+
+            driver.FindElement(By.XPath($"//div[@id='{controlName}-dropmenu']//li[text()='{value}']")).Click();
+        }
+    }
+}
---
+++ MySelenium/UnitTest1.cs
@@ -0,0 +1,31 @@
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+
+namespace MySelenium
+{
+    public class Tests : DriverHelper
+    {
+        
+        [SetUp]
+        public void Setup()
+        {
+            
+            driver = new ChromeDriver();
+        }
+
+        [Test]
+        public void Test1()
+        {
+            driver.Navigate().GoToUrl("https://demowf.aspnetawesome.com");
+            driver.FindElement(By.Id("ContentPlaceHolder1_Meal")).SendKeys("Tomato");
+            driver.FindElement(By.XPath("//input[@name='ctl00$ContentPlaceHolder1$ChildMeal1']/following-sibling::div[text()='Celery']")).Click();
+            CustomControl.comboControl("ContentPlaceHolder1_AllMealsCombo", "Almonds");
+            
+            
+            Console.WriteLine("TestExecution");
+            Assert.Pass();
+        }
+    }
+}
